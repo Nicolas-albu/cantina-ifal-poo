944dd88eb7e961cf4fc419cd724de2ab958af40b
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-05-10 00:57:22 -0300

    Adição da Model responsável pelo cadastro de produtos no estoque

---
+++ app/src/main/java/com/ifal/cantina/model/RegisterItemModel.java
@@ -0,0 +1,15 @@
+package com.ifal.cantina.model;
+
+import com.ifal.cantina.interfaces.AModel;
+
+public class RegisterItemModel extends AModel {
+    
+    public RegisterItemModel() {
+        super();
+    }
+
+    @Override
+    public Object add(Object object) {
+        return null;
+    }
+}
