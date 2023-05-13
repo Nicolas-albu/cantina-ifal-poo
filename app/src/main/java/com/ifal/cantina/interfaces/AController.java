c30131351f8ac4adb9faf18a992f01d0a1639d74
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-05-13 19:30:10 -0300

    Modificação para sobrecarga de construtor

--- app/src/main/java/com/ifal/cantina/interfaces/AController.java
+++ app/src/main/java/com/ifal/cantina/interfaces/AController.java
@@ -1,5 +1,6 @@
 package com.ifal.cantina.interfaces;
 
+import com.ifal.cantina.annotations.Overload;
 import com.ifal.cantina.utils.factorys.ModelFactory;
 import com.ifal.cantina.utils.factorys.ViewFactory;
 
@@ -7,9 +8,16 @@ public abstract class AController {
     protected AModel model;
     protected AView view;
 
-    public AController(AModel model, AView view) {
+    @Overload
+    public AController(){
         this.model = ModelFactory.createModel(this);
         this.view = ViewFactory.createView(this);
+    }
+
+    @Overload
+    public AController(AModel model, AView view) {
+        this.model = model;
+        this.view = view;
     }
 
     public abstract void run();
