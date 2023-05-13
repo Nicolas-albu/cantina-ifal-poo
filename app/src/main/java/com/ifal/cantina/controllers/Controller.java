86791b47f10fb195dc38f3fea4c4a269f1d3aa0c
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-05-13 19:40:24 -0300

    Remoção de argumentos para criação do RegisterItemController para utilizar construtor vázio

--- app/src/main/java/com/ifal/cantina/controllers/Controller.java
+++ app/src/main/java/com/ifal/cantina/controllers/Controller.java
@@ -1,25 +1,25 @@
 package com.ifal.cantina.controllers;
 
 import com.ifal.cantina.interfaces.AController;
-import com.ifal.cantina.interfaces.AModel;
-import com.ifal.cantina.interfaces.AView;
 
 public class Controller extends AController {
 
-    public Controller(AModel model, AView view) {
-        super(model, view);
+    public Controller() {
+        super();
     }
 
     @Override
     public void run() {
         int resultShowMenu = (Integer) super.view.show();
-        
+
         switch (resultShowMenu) {
             case 9:
                 break;
             case 1:
-                new RegisterItemController(super.model, super.view).run();
+                new RegisterItemController().run();
                 break;
+//            case 2:
+
         }
     }
 }
