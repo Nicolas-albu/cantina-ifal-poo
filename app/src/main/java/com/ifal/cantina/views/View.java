43f9ab0e105562076fda93695d28b2b640a77a37
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-05-14 15:07:01 -0300

    Reformatação de View.java

--- app/src/main/java/com/ifal/cantina/views/View.java
+++ app/src/main/java/com/ifal/cantina/views/View.java
@@ -1,8 +1,8 @@
 package com.ifal.cantina.views;
 
+import com.ifal.cantina.interfaces.AView;
 import com.ifal.cantina.utils.HandleInput;
 import com.ifal.cantina.utils.ReadProperties;
-import com.ifal.cantina.interfaces.AView;
 
 /**
  * Subclass of AView which is the main/initial View, for selecting menu options.
@@ -17,7 +17,7 @@ public class View extends AView {
     public View() {
         super();
     }
-    
+
     @Override
     public Object show() {
         String key_property;
@@ -29,7 +29,7 @@ public class View extends AView {
         System.out.println("===================================================\n");
         System.out.println("Selecione uma das seguintes opcoes: ");
 
-        for(int index_menu = 0; index_menu < quantity_options; index_menu++) {
+        for (int index_menu = 0; index_menu < quantity_options; index_menu++) {
             key_property = "show.menu." + (index_menu + 1);
 
             System.out.printf("[%s] - %s%n",
