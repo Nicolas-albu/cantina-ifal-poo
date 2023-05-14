e489560cb217c975cdb760479dece17440540f4a
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-05-14 05:55:27 -0300

    Adição da inserção de produtos no banco.

--- app/src/main/java/com/ifal/cantina/controllers/RegisterItemController.java
+++ app/src/main/java/com/ifal/cantina/controllers/RegisterItemController.java
@@ -1,8 +1,10 @@
 package com.ifal.cantina.controllers;
 
+import com.ifal.cantina.exceptions.EntityException;
 import com.ifal.cantina.interfaces.AController;
-import com.ifal.cantina.views.RegisterItemView;
 import com.ifal.cantina.entities.Product;
+
+import java.sql.SQLException;
 
 /**
  * Subclass of AController to control item registration.
@@ -21,7 +23,33 @@ public class RegisterItemController extends AController {
     
     @Override
     public void run() {
-        Product product = (Product) new RegisterItemView().show();
-        super.model.insert(product);
+        Product product = this.getProduct();
+
+        try {
+            super.model.insert(product).commit();
+
+        } catch (IllegalAccessException | SQLException error) {
+            this.view.printException(error);
+        }
+
+        this.view.close();
+    }
+
+    /**
+     * Returns the obtained Product object by invoking the view's show() method.
+     *
+     * @return The Product object obtained from the view or null in case of an exception.
+     */
+    private Product getProduct() {
+        Product product = null;
+
+        try {
+            product = (Product) this.view.show();
+
+        } catch (EntityException error) {
+            this.view.printException(error);
+        }
+
+        return product;
     }
 }
