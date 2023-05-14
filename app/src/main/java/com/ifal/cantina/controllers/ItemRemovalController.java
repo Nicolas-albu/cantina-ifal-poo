f665849b23ce89fda552f101fab036fa963015ec
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-05-14 18:11:23 -0300

    Adição de um Controller para remoção de itens no estoque

---
+++ app/src/main/java/com/ifal/cantina/controllers/ItemRemovalController.java
@@ -0,0 +1,59 @@
+package com.ifal.cantina.controllers;
+
+import com.ifal.cantina.exceptions.EntityException;
+import com.ifal.cantina.interfaces.AController;
+import com.ifal.cantina.entities.Product;
+
+import java.sql.SQLException;
+
+/**
+ * <p> The ItemRemovalController class is a subclass of AController that handles the removal of a
+ *     product item.</p>
+ * <p> It interacts with the model and view components to execute the removal logic.</p>
+ *
+ * @author Nicolas Albuquerque R.
+ * @see AController
+ */
+public class ItemRemovalController extends AController {
+
+    /**
+     * Constructs a new instance of the ItemRemovalController class.
+     * Initializes the model and view using the parent class constructor.
+     */
+    public ItemRemovalController() {
+        super();
+    }
+
+    @Override
+    public void run() {
+        Product product = this.getProduct();
+
+        try {
+            super.model.delete(product).commit();
+
+        } catch (IllegalAccessException | SQLException error) {
+            super.view.printException(error);
+        }
+
+        super.view.printResult();
+        super.view.close();
+    }
+
+    /**
+     * Retrieves the product from the view.
+     *
+     * @return The product selected for removal.
+     */
+    private Product getProduct() {
+        Product product = null;
+
+        try {
+            product = (Product) super.view.show();
+
+        } catch (EntityException error) {
+            this.view.printException(error);
+        }
+
+        return product;
+    }
+}
