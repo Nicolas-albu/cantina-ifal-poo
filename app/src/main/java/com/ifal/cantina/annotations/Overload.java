470b804d0fde192d77b8c8b4c568e9dd950c1795
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-05-14 18:12:59 -0300

    Reformatação do JavaDoc de Overload.java

--- app/src/main/java/com/ifal/cantina/annotations/Overload.java
+++ app/src/main/java/com/ifal/cantina/annotations/Overload.java
@@ -1,14 +1,16 @@
 package com.ifal.cantina.annotations;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that a method or constructor is an overloaded version of another method or constructor.
- * This annotation serves as a documentation to indicate the purpose of the method or constructor.
+ * <p> Indicates that a method or constructor is an overloaded version of another method or
+ * constructor.</p>
+ * <p> This annotation serves as a documentation to indicate the purpose of the method or
+ * constructor.</p>
  *
  * @author Nicolas Albuquerque R.
  */
