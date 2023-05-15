bd0d46958e1fc08c624fe5e42576653666175b3d
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-05-14 23:26:08 -0300

    Correção do nome da ItemRegisterController

---
+++ app/src/main/java/com/ifal/cantina/controllers/ItemRegisterController.java
@@ -0,0 +1,57 @@
+package com.ifal.cantina.controllers;
+
+import com.ifal.cantina.exceptions.EntityException;
+import com.ifal.cantina.interfaces.AController;
+import com.ifal.cantina.entities.Product;
+
+import java.sql.SQLException;
+
+/**
+ * Subclass of AController to control item registration.
+ *
+ * @author Nicolas Albuquerque R.
+ * @see AController
+ */
+public class ItemRegisterController extends AController {
+
+    /**
+     * Constructs a new instance of the ItemRegisterController class.
+     * Initializes the model and view using the parent class constructor.
+     */
+    public ItemRegisterController() {
+        super();
+    }
+    
+    @Override
+    public void run() {
+        Product product = this.getProduct();
+
+        try {
+            super.model.insert(product).commit();
+
+        } catch (IllegalAccessException | SQLException error) {
+            super.view.printException(error);
+        }
+
+        super.view.printResult();
+        super.view.close();
+    }
+
+    /**
+     * Returns the obtained Product object by invoking the view's show() method.
+     *
+     * @return The Product object obtained from the view or null in case of an exception.
+     */
+    private Product getProduct() {
+        Product product = null;
+
+        try {
+            product = (Product) super.view.show();
+
+        } catch (EntityException error) {
+            super.view.printException(error);
+        }
+
+        return product;
+    }
+}
